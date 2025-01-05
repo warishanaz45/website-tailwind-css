b40c0722fb301a21d31c45b957e2545e49555776
warishanaz45 <warishanaz188@gmail.com>
2025-01-05 18:44:57 +0500

    first commit

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -1,23 +1,39 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import AOS from 'aos';
-import { useEffect } from 'react';
 import 'aos/dist/aos.css';
 
 const HomeContent = () => {
   useEffect(() => {
-    AOS.init({
-      duration: 1000, 
-      once: true,     
-    });
+    if (typeof window !== "undefined") {
+      AOS.init({
+        duration: 1000, // Animation duration in milliseconds
+        once: true, // Animation runs only once
+      });
+    }
   }, []);
+
   return (
-    <section className="hero h-screen flex items-center bg-cover bg-center" style={{ backgroundImage: "url('/WhatsApp Image 2024-12-20 at 9.27.25 PM.jpeg')" }}>
+    <section
+      className="hero h-screen flex items-center bg-cover bg-center"
+      style={{
+        backgroundImage: "url('/background.jpeg')",
+      }}
+    >
       <div className="pl-0 md:pl-16 text-white text-center">
-        <h1 className="text-6xl font-bold drop-shadow-md" data-aos="fade-up">Welcome to Watch World</h1>
-        <p className="text-2xl mt-4">Your one-stop shop for the best watches</p>
-        <button className="mt-6 px-6 py-3 bg-blue-500 hover:bg-blue-700 rounded-md font-bold">Shop Now</button>
+        <h1
+          className="text-6xl font-bold drop-shadow-md"
+          data-aos="fade-up"
+        >
+          Welcome to Watch World
+        </h1>
+        <p className="text-2xl mt-4">
+          Your one-stop shop for the best watches
+        </p>
+        <button className="mt-6 px-6 py-3 bg-blue-500 hover:bg-blue-700 rounded-md font-bold">
+          Shop Now
+        </button>
       </div>
     </section>
   );
